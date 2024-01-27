2b52acfb03ae8fa9782a22dab27fbe6419ca1684
Samidha Prashant Ramteke <86358930+Samidha07@users.noreply.github.com>
2024-01-27 23:15:10 +0530

    Create Arithmetic.sh
    Create an executable Bash script that prompts the user for two integers, then stores and prints both the sum and product of the two integers.

---
+++ Arithmetic.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "Enter first number"
+read first
+echo "Enter second number"
+read second
+
+echo "Sum of two integers is : $(($first + $second))"
+echo "Product of two integers is : $(($first * $second))"
